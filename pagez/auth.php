81ab8ba2d23a6a942a13569ecc9909d4c0dbafe5
Sander Eraser <bamboomy@gmail.com>
2019-12-24 22:15:40 +0100

    to auth or not to auth

---
+++ pagez/auth.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  /*whatever width you want*/
+}
+</style>
+</head>
+<body>
+<center>
+<div class="outer">
+  <div class="middle">
+    <div class="inner">
+<form action="own.html" method="post">
+		Username:&nbsp
+		<input type="password" name="password" />
+		<input type="submit" value="Log in">
+		<input type="hidden" name="token" value="token">
+</form> 	
+	</div>
+  </div>
+</div>
+</center>
+</body>
+</html>
