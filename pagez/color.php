a2acc47f2eea4d345eec5a0977609b22b6bfaca2
Sander Eraser <bamboomy@gmail.com>
2019-12-25 07:49:55 +0100

    early in the mor-ning

--- pagez/color.php
+++ pagez/color.php
@@ -10,13 +10,11 @@ $three = array("the dark", "elves");
 
 $sentence = $one[rand(0, count($one))] . ' ' . $two[rand(0, count($two))] . ' ' . $three[rand(0, count($three))];
 
-/*
 echo rand(0, count($one));
 
 echo rand(0, count($two));
 
 echo rand(0, count($three));
-*/
 
 ?>
 
