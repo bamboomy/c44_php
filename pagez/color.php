eb8665183e255464343e640e794766c91b35ce43
Sander Eraser <bamboomy@gmail.com>
2019-12-25 07:24:35 +0100

    early in the mor-ning

--- pagez/color.php
+++ pagez/color.php
@@ -29,6 +29,48 @@ session_start();
   width: 400px;
   /*whatever width you want*/
 }
+
+.container {
+	position: relative;
+  width: 15%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+.text {
+  color: white;
+  font-size: 15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+
 </style>
 </head>
 <body>
@@ -39,10 +81,25 @@ session_start();
 		<h1>Hey <? echo $_SESSION['user']; ?>,</h1>
 		<h3>Choose your color:</h3>
 		<br/>
-		<img src="../imgz/yellow.png" alt="Smiley face" height="42" width="42">
-		<img src="../imgz/grey.png" alt="Smiley face" height="42" width="42"><br/>
-		<img src="../imgz/green.png" alt="Smiley face" height="42" width="42">
-		<img src="../imgz/grey.png" alt="Smiley face" height="42" width="42"><br/>
+		
+			<div class="container">
+			  <img src="../imgz/grey.png" alt="Avatar" class="image">
+			  <div class="overlay">
+				<div class="text">Already taken</div>
+			  </div>
+			</div>		
+			<div class="container">
+			  <img src="../imgz/yellow.png" alt="Avatar" class="image">
+			</div>					
+			<div class="container">
+			  <img src="../imgz/green.png" alt="Avatar" class="image">
+			</div>		
+			<div class="container">
+			  <img src="../imgz/grey.png" alt="Avatar" class="image">
+			  <div class="overlay">
+				<div class="text">Already taken</div>
+			  </div>
+			</div>					
 	</div>
   </div>
 </div>
