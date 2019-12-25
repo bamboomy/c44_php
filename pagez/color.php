cb2baa84690f5df0200d46b9f074d2463697968e
Sander Eraser <bamboomy@gmail.com>
2019-12-25 06:38:16 +0100

    early in the mor-ning

---
+++ pagez/color.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+
+
+?>
+
+<html>
+<head>
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  /*whatever width you want*/
+}
+</style>
+</head>
+<body>
+<center>
+<div class="outer">
+  <div class="middle">
+    <div class="inner">
+		<h1>Hey <? echo $_SESSION['user']; ?>,</h1>
+		<h3>Choose your color:</h3>
+		<br/>
+		<img src="../imgz/yellow.gif" alt="Smiley face" height="42" width="42">
+		<img src="../imgz/red.gif" alt="Smiley face" height="42" width="42"><br/>
+		<img src="../imgz/green.gif" alt="Smiley face" height="42" width="42">
+		<img src="../imgz/blue.gif" alt="Smiley face" height="42" width="42"><br/>
+	</div>
+  </div>
+</div>
+</center>
+</body>
+</html>
