5434444929fcc4e0f2cac263823a85737c2cbb93
Sander Eraser <bamboomy@gmail.com>
2019-12-25 06:26:26 +0100

    early in the mor-ning

--- pagez/lobby.php
+++ pagez/lobby.php
@@ -39,7 +39,7 @@ session_start();
 		<h1>Welcome <? echo $_SESSION['user']; ?>,</h1>
 		<h3>What do you want to do?</h3>
 		<br/>
-		<a href="test.php">Create a game</a><br/>
+		<a href="color.php">Create a game</a><br/>
 		<br/>
 		<a href="test.php">Join (hash1)</a><br/><br/>
 		<a href="test.php">Join (hash2)</a><br/><br/>
