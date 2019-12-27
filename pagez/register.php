4100a39e4ed0352f531cd3de1c5d20f15e5a2b1e
Sander Eraser <bamboomy@gmail.com>
2019-12-27 18:34:29 +0100

    va-kan-tieieie

---
+++ pagez/register.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  /*whatever width you want*/
+}
+</style>
+
+	</head>
+<body>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=469961633832839&autoLogAppEvents=1"></script>
+
+	<div class="outer">
+		<div class="middle">
+			<div class="inner">
+
+				<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
+
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
