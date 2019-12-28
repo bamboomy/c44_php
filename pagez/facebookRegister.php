36ea318a579f4e8a65536533b8650cbc56381a52
Sander Theetaert <sander.theetaert@realdolmen.com>
2019-12-28 20:13:25 +0100

    metafoor

--- pagez/facebookRegister.php
+++ pagez/facebookRegister.php
@@ -104,6 +104,8 @@ include_once("settings.php");
 				<br/>
 				Care to choose your destiny? (oops, I mean name?):<br/>
 				<br/>
+<form action="login.php" method="post">
+
 <? 
 
 $four = array("Ma", "Ze", "Di", "Ka", "Fo", "Zi", "Lo", "Je", "Ri", "A", "E", "O");
@@ -124,7 +126,10 @@ for($i=0; $i<5; $i++){
 	echo "<div class='left'><input type='radio' name='name' value='".$name[$i]."'>".$name[$i]."</div>";
 }
 ?>
-				<div class='left'><input type='radio' name='name' onclick="alert('test');" value='own'>I want to choose my own name.&nbsp;<input type="text" name="password" /></div>
+				<div class='left'><input type='radio' name='name' value='own'>I want to choose my own name:&nbsp;<input type="text" name="password" /></div>
+		<input type="submit" value="Register">
+		<input type="hidden" name="token" value="token">
+</form> 	
 			</div>
 			</div>
 	</div>
