c3b40b03aa700308e585ba955e19f2d85d9ab196
Sander Theetaert <sander.theetaert@realdolmen.com>
2019-12-28 20:05:50 +0100

    metafoor

--- pagez/facebookRegister.php
+++ pagez/facebookRegister.php
@@ -124,6 +124,7 @@ for($i=0; $i<5; $i++){
 	echo "<div class='left'><input type='radio' name='name' value='".$name[$i]."'>".$name[$i]."</div>";
 }
 ?>
+				<div class='left'><input type='radio' name='name' value='own'>I want to choose my own name.</div>
 			</div>
 			</div>
 	</div>
