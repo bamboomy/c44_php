d8a57b9de65c14e0d28d8293254e09fc9c88ae7c
Sander Eraser <bamboomy@gmail.com>
2019-12-29 11:27:45 +0100

    home again

--- pagez/safeFacebook.php
+++ pagez/safeFacebook.php
@@ -2,19 +2,93 @@
 
 session_start();
 
-include_once("settings.php");
+if(!isset($_SESSION['fbId'])){
+	
+	header("Location: register.php");
+		
+	exit;
+}
 
-$sql = "insert into gebruiker (name, fbId) values (";
+include_once("settings.php");
 
 if($_POST['name'] == 'own'){
 	
-	$sql .= $_POST['ownName'];
+	$c44Name = test_input($_POST['ownName']);
 	
 } else {
 	
-	$sql .= $_POST['name'];
+	$c44Name = test_input($_POST['name']);
 }
 
-$sql .= ", )";
+$sql = "insert into gebruiker (voornaam, name, fbId) values ('".$_SESSION['firstName']."', '".$c44Name."', '".$_SESSION['fbId']."');";
+
+$result = $conn->query($sql);
 
 ?>
+<html>
+<head>
+
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+}
+
+.center {
+  margin: 0 auto;
+  text-align: center;
+}
+
+.left{
+
+		text-align: left;
+}
+
+.right {
+	
+	text-align: right;
+}
+
+</style>
+
+<script>
+
+</script>
+
+
+	</head>
+<body>
+
+	<div class="outer">
+		<div class="middle">
+			<div class="inner center">
+
+				<h2>Hey <? echo $_SESSION['firstName']; ?>,</h2>
+				
+				<? echo "You will be known as '".$c44Name."' from now on..."; ?><br/>
+				<br/>
+				May your actions be just, fierce and fair!!!<br/>
+				<br/>
+				<a href='register.php'>I shall continue</a>
+				
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
