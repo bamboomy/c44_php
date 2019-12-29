23a9fd8938956862d8e3afa25a3917edd7b0c292
Sander Eraser <bamboomy@gmail.com>
2019-12-29 13:02:11 +0100

    home again

--- pagez/invite.php
+++ pagez/invite.php
@@ -6,6 +6,74 @@ $_SESSION['hash'] = $_GET['game'];
 
 $_SESSION['invited'] = true;
 
-header("Location: auth.php");
+if(isset($_SESSION['id'])){
+	
+	header("Location: color.php");
+		
+	exit;
+}
 
 ?>
+<html>
+<head>
+
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+}
+
+.center {
+  margin: 0 auto;
+  text-align: center;
+}
+
+.left{
+
+		text-align: left;
+}
+
+.right {
+	
+	text-align: right;
+}
+
+</style>
+
+<script>
+
+</script>
+
+
+	</head>
+<body>
+
+	<div class="outer">
+		<div class="middle">
+			<div class="inner center">
+
+				<h2>Hellow there,</h2>
+				
+				You need to be <a href="register.php">registered</a> to be able to participate in a game...
+
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
