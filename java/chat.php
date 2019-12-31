963d49ca4f5f41ee76f8235267f4d1429de6e8bc
Sander Eraser <bamboomy@gmail.com>
2019-12-31 18:55:42 +0100

    newyears eve

--- java/chat.php
+++ java/chat.php
@@ -5,6 +5,8 @@ session_start();
 header("Access-Control-Allow-Origin: http://chess4four.io:8080");
 header("Access-Control-Allow-Credentials: true");
 
+include_once("../pagez/settings.php");
+
 if(!isset($_SESSION['id'])){
 	
 	header("Location: register.php");
@@ -12,6 +14,17 @@ if(!isset($_SESSION['id'])){
 	exit;
 }
 
-echo $_POST['text'] . "=>" . $_GET['board'] ;
+$sql = "select name, game from colors_taken where java_hash = '".test_input($_GET['board'])."';";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+$sql = "insert into chat (game, text) ";
+$sql .= " values ('".$row['game']."', '".$row['name'].": ".  test_input($_POST['text'])."');";
+
+$result = $conn->query($sql);
+
+echo "success";
 
 ?>
