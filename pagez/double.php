681166a1fc0278be6f655f078e2653f636301054
Sander Theetaert <sander.theetaert@realdolmen.com>
2019-12-31 14:06:16 +0100

    blauwe kater

---
+++ pagez/double.php
@@ -0,0 +1,119 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['id'])){
+	
+	header("Location: register.php");
+		
+	exit;
+}
+?>
+
+<html>
+<head>
+<style>
+.outer {
+  display: table;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.middle {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  /*whatever width you want*/
+}
+
+.container {
+	position: relative;
+  width: 15%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.overlay_orig {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  color: white;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+.container:hover .overlay_orig {
+  opacity: 0;
+  transition: .5s ease;
+}
+
+.text {
+  color: white;
+  font-size: 15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+
+</style>
+
+<script src="../js/jquery-3.4.1.min.js"></script>
+
+<script>
+
+
+</script>
+
+</head>
+<body>
+<center>
+<div class="outer">
+  <div class="middle">
+    <div class="inner">
+		<h1>Hey <? echo $_SESSION['name']; ?>,</h1>
+
+			You can't join the same game twice...<br/>
+			<br/>
+			<a href="">Let's see which other games you can join.</a>
+	</div>
+  </div>
+</div>
+</center>
+</body>
+</html>
