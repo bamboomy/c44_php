4a0c7768e8bbee20c67360cac62554f43a4ab57e
Sander Eraser <bamboomy@gmail.com>
2019-12-31 17:58:17 +0100

    newyears eve

--- java/chat.php
+++ java/chat.php
@@ -2,14 +2,14 @@
 
 session_start();
 
-/*
+header("Access-Control-Allow-Origin: *");
+
 if(!isset($_SESSION['id'])){
 	
 	header("Location: register.php");
 		
 	exit;
 }
-*/
 
 echo $_POST['text'];
 
