d78e621d89219d5515dd3e1e40fb0e4e6592cdc7
Sander Eraser <bamboomy@gmail.com>
2019-12-31 19:07:13 +0100

    newyears eve

---
+++ java/chatText.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+header("Access-Control-Allow-Origin: http://chess4four.io:8080");
+header("Access-Control-Allow-Credentials: true");
+
+include_once("../pagez/settings.php");
+
+if(!isset($_SESSION['id'])){
+	
+	header("Location: register.php");
+		
+	exit;
+}
+
+$sql = "select name, game from colors_taken where java_hash = '".test_input($_GET['board'])."';";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+$sql = "select text from chat where game = '".$row['game']."';";
+
+$result = $conn->query($sql);
+
+while ($row = $result->fetch_assoc()) {
+
+	echo $row['text'] . "<br/>";
+}
+
+?>
