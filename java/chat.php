a9880879ec4fe96c1f6f7e978b1e8a19b9b6a22c
Sander Eraser <bamboomy@gmail.com>
2019-12-31 18:43:48 +0100

    newyears eve

--- java/chat.php
+++ java/chat.php
@@ -5,17 +5,13 @@ session_start();
 header("Access-Control-Allow-Origin: http://chess4four.io:8080");
 header("Access-Control-Allow-Credentials: true");
 
-var_dump($_SESSION);
-
-/*
 if(!isset($_SESSION['id'])){
 	
 	header("Location: register.php");
 		
 	exit;
 }
-*/
 
-echo $_POST['text'];
+echo $_POST['text'] . "=>" . $_GET['board'] ;
 
 ?>
