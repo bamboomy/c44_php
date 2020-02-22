5c335a8636a3ace00209af3ba00ddd3c5bd92b9e
Sander <bamboomy@gmail.com>
2020-02-22 02:23:13 +0100

    cookie

--- pagez/color.php
+++ pagez/color.php
@@ -167,7 +167,7 @@ again();
 <center>
 <div class="outer">
   <div class="middle">
-    <div class="inner center">
+    <div class="inner">
 		<h1>Hey <? echo $_SESSION['name']; ?>,</h1>
 		<h3>We're creating game:</h3>
 		<h3><? echo $_SESSION['sentence']; ?></h3>
