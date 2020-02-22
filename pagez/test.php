2adbc564a11cb0f7abef6c50cac742abd661f4ea
Sander <bamboomy@gmail.com>
2020-02-22 05:10:09 +0100

    test

--- pagez/test.php
+++ pagez/test.php
@@ -1,5 +1,9 @@
 <?
 
-include_once("http://chess4four.io:8080/?id=test");
+session_start();
+
+$_SESSION['colorValues'] = array( 'firsthash' => 'green' , 'firsthash2' => 'yellow', 'firsthash3' => 'blue');
+
+echo "<a href='test2.php?hash=".'firsthash'."'>test</a>"
 
 ?>
