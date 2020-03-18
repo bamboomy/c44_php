7780fdcddd3b29e36659691a144c2afc07f15575
Sander <bamboomy@gmail.com>
2020-03-18 20:31:25 +0100

    bots

--- dev/pagez/bots.php
+++ dev/pagez/bots.php
@@ -1,20 +1,51 @@
 <html>
 	<head>
 		<style>
-		.center {
-			position:absolute; 
-			left:0; right:0;
-			top:0; bottom:0;
-			margin:auto;
-		}
-		</style>
+.outer {
+	display: table;
+	
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 100%;
+}
+
+.middle {
+	display: table-cell;
+	vertical-align: middle;
+}
+
+.inner {
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+}
+
+.center {
+  margin: 0 auto;
+  text-align: center;
+}
+
+</style>
+
 	</head>
 <body>
 
 	<center>
+
+	<div class="outer">
+		<div class="middle">
+			<div class="inner center">
+
 	<a href="pagez/bots.php">
 		<div class="center">Try</div>
 	</a>
+
+			</div>
+		</div>
+	</div>
+
 	</center>
 
 </body>
