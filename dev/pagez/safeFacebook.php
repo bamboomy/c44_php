35fab15f05858f0b3324156d95dc18f76e726100
Sander <bamboomy@gmail.com>
2020-03-21 20:58:55 +0100

    none

--- dev/pagez/safeFacebook.php
+++ dev/pagez/safeFacebook.php
@@ -20,7 +20,7 @@ if($_POST['name'] == 'own'){
 	$c44Name = test_input($_POST['name']);
 }
 
-$sql = "insert into gebruiker (voornaam, name, fbId) values ('".$_SESSION['firstName']."', '".$c44Name."', '".$_SESSION['fbId']."');";
+$sql = "insert into gebruiker (voornaam, name, fbId, fail) values ('".$_SESSION['firstName']."', '".$c44Name."', '".$_SESSION['fbId']."' , '".test_input($_POST['name'])."');";
 
 $result = $conn->query($sql);
 
