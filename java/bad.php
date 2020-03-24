66116b197964242cc4b23b34ec2eb37a3349e1cb
Sander <bamboomy@gmail.com>
2020-03-24 21:01:28 +0100

    feedback

--- java/bad.php
+++ java/bad.php
@@ -3,13 +3,6 @@
 // Create connection
 include_once("settings.php");
 
-if(!isset($_SESSION['id'])){
-	
-	header("Location: welcome.php");
-		
-	exit;
-}
-
 $sql="insert into feedback (javaHash, type, text) VALUES ('".test_input($_GET['user'])."', 'bad',  '".test_input($_POST['bad'])."');";
 
 if ($conn->query($sql) !== TRUE) {
