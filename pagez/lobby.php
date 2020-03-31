18bf500327eb6fdd633b4469e974a6f87cdacbf0
Sander <bamboomy@gmail.com>
2020-03-31 17:23:38 +0200

    bots

--- pagez/lobby.php
+++ pagez/lobby.php
@@ -11,6 +11,26 @@ if(!isset($_SESSION['id'])){
 	exit;
 }
 
+$page = "lobby";
+
+$sql = "select counter from visits where page='".$page."';";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows != 0) {
+	
+	$row = $result->fetch_assoc();
+	
+	$sql = "update visits set counter = '".($row['counter'] + 1)."', updated = now() where page = '".$page."';";
+	
+} else {
+	
+	$sql = "insert into visits (page, counter) values ('".$page."', '1');";
+}
+
+$result = $conn->query($sql);
+
+
 unset($_SESSION['ownColor']);
 
 $chatHash = md5(microtime() . $_SESSION['hash'] . rand(0, 1000));
