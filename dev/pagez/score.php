d602a413acf82dd1ff6620a39d4a1a4647fb8509
Sander <bamboomy@gmail.com>
2020-08-19 19:45:02 +0200

    temp

--- dev/pagez/score.php
+++ dev/pagez/score.php
@@ -1,5 +1,24 @@
-<?
+<?php
 
-echo "score :)";
+session_start();
+
+if(!isset($_SESSION['id'])){
+	
+	header("Location: welcome.php");
+		
+	exit;
+}
+
+include_once("settings.php");
+
+$sql = "select reason from game_result where game = '".test_input($_GET['game'])."';";
+
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()){
+	
+	echo $row['reason'];
+	
+}
 
 ?>
