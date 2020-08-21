f7bdb6c918191eb1c5d1b2b1a58725319c3955eb
Sander <bamboomy@gmail.com>
2020-08-21 20:20:23 +0200

    reload

--- dev/pagez/score.php
+++ dev/pagez/score.php
@@ -28,13 +28,7 @@ $row2 = $result2->fetch_assoc();
 		<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
 		
 		<link rel="stylesheet" type="text/css" href="../css/default.css">
-		
-		<link rel="stylesheet" href="../frameworkz/bootstrap-4.4.1-dist/css/bootstrap.css">
-		
-		<script src="../js/jquery-3.4.1.min.js" ></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="../frameworkz/bootstrap-4.4.1-dist/js/bootstrap.bundle.js"></script>
-		
+
 <style>
 
 	#reload{
