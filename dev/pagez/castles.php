7fe0adc416ce9b57509dceae6373706aa2384000
Sander <bamboomy@gmail.com>
2020-08-29 19:43:04 +0200

    castles

--- dev/pagez/castles.php
+++ dev/pagez/castles.php
@@ -274,7 +274,7 @@ function again() {
 //again();
 
 var now = new Date();
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 45, 0, 0) - now;
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 44, 0, 0) - now;
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
 }
