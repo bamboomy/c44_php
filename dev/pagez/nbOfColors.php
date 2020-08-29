b4a7424881c727e73babd139dbce81999b680a5d
Sander <bamboomy@gmail.com>
2020-08-29 17:21:14 +0200

    castles

--- dev/pagez/nbOfColors.php
+++ dev/pagez/nbOfColors.php
@@ -13,6 +13,8 @@ include_once("settings.php");
 
 $sql = "SELECT COUNT (DISTINCT color) from colors_taken where game = '".test_input($_SESSION['hash'])."';";
 
+echo $sql."<br/>";
+
 $result = $conn->query($sql) or die($conn->error);
 
 $row = $result->fetch_row();
