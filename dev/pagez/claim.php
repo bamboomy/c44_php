fcbf3c7f95fb3b53164f146636a9fdf5a0e800c7
Sander <bamboomy@gmail.com>
2020-08-29 13:00:41 +0200

    castles

--- dev/pagez/claim.php
+++ dev/pagez/claim.php
@@ -11,12 +11,12 @@ if(!isset($_SESSION['id'])){
 
 include_once("settings.php");
 
-$color = $_GET['color'];
+$color = ;
 
 $java_hash = md5($_SERVER['REMOTE_ADDR'] . microtime() . $_SESSION['hash'] . $_SESSION['id']);
 
 $sql = "insert into colors_taken (game, color, name, java_hash) ";
-$sql .= " values ('".$_SESSION['hash']."', '".$_SESSION['ownColor']."', '".$_SESSION['name']."', '".$java_hash."');";
+$sql .= " values ('".$_SESSION['hash']."', '".test_input($_GET['color'])."', '".$_SESSION['name']."', '".$java_hash."');";
 
 $conn->query($sql) or die($conn->error);
 
