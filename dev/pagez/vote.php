dedf1ea8f4954110462f63f53c1e13a845db69c6
Sander <bamboomy@gmail.com>
2020-08-30 10:18:40 +0200

    castles

--- dev/pagez/vote.php
+++ dev/pagez/vote.php
@@ -11,10 +11,21 @@ if(!isset($_SESSION['id'])){
 
 include_once("settings.php");
 
-$sql = "insert into votes (game, javaHash, value) values ('".$_SESSION['hash']."', '".$_SESSION['java_hash']."', '".test_input($_GET['value'])."');";
+$sql = "select id from votes where game = '".$_SESSION['hash']."' and javaHash = '".$_SESSION['java_hash']."';";
 
 $result = $conn->query($sql) or die($conn->error);
 
-echo "succezzzz :)";
+if($result->num_rows == 0){
+
+	$sql = "insert into votes (game, javaHash, value) values ('".$_SESSION['hash']."', '".$_SESSION['java_hash']."', '".test_input($_GET['value'])."');";
+
+	$result = $conn->query($sql) or die($conn->error);
+	
+	echo "succezzzz :)";
+
+} else {
+
+	echo "double :(";
+}
 
 ?>
