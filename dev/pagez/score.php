8848ac51e96b7053f59f3685042c3c8270b3cb9c
Sander <bamboomy@gmail.com>
2020-12-31 22:01:39 +0100

    pindakaas

--- dev/pagez/score.php
+++ dev/pagez/score.php
@@ -461,6 +461,8 @@ while($row = $result->fetch_assoc()){
 		
 		$players[$row['reason']] = $temp;
 		
+		var_dump($temp);
+		
 	} else {
 		
 		$players[$row['reason']] = array($row3['color'], str_replace("Random85247", "Bot", str_replace("Dubious85247", "Dubious", $row3['name'])));
