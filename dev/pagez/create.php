17202ed0f62a1decf47358f3a7e82ec3d3f43c3e
Sander <bamboomy@gmail.com>
2021-05-23 14:56:38 +0200

    dev

--- dev/pagez/create.php
+++ dev/pagez/create.php
@@ -11,17 +11,11 @@ if(!isset($_SESSION['id'])){
 
 include_once("settings.php");
 
-$four = array("A wonderfull", "Some good", "A tea spoon of", "A green", "A wooden", "A bright", "A decent", "An excellent", "A handfull of");
+$four = array("Alfa", "Beta", "Gamma", "Delta", "Jota", "Mu", "Pi", "Rho", "Sigma", "Tau", "Phi", "Chi", "Psi", "Omega");
 
-$one = array("breeze", "tea", "outlet", "garden", "color", "t-shirt", "glass", "chocolate", "ashtray", "card", "letter", "globe", "bottle");
+$_SESSION['sentence'] = '"Game ' . $four[rand(0, count($four) - 1)] . '-';
 
-$two = array("without", "in", "between", "amongst", "outside of", "with");
-
-$three = array("the dark", "elves", "Godot", "a lamp", "the unknown", "the French", "a sister", "some coffee", "a group of Elvises", "Indiana Jones");
-
-$_SESSION['sentence'] = '"' . $four[rand(0, count($four) - 1)] . ' ';
-
-$_SESSION['sentence'] .= $one[rand(0, count($one) - 1)] . '<br/>' . $two[rand(0, count($two) - 1)] . ' ' . $three[rand(0, count($three) - 1)] . '."' ;
+$_SESSION['sentence'] .= rand(1000, 7000) . '.' . rand(50, 600) . '"';
 
 $failCounter = 0;
 
@@ -35,9 +29,9 @@ if ($result->num_rows != 0) {
 	
 	while($fail){
 		
-		$_SESSION['sentence'] = '"' . $four[rand(0, count($four) - 1)] . ' ';
+		$_SESSION['sentence'] = '"Game ' . $four[rand(0, count($four) - 1)] . '-';
 
-		$_SESSION['sentence'] .= $one[rand(0, count($one) - 1)] . '<br/>' . $two[rand(0, count($two) - 1)] . ' ' . $three[rand(0, count($three) - 1)] . '."' ;
+		$_SESSION['sentence'] .= rand(1000, 7000) . '.' . rand(50, 600) . '"';
 
 		$sql = "select id from game where sentence = '".$_SESSION['sentence']."';";
 
