7dcf49878342a879ae5c0fbd132acfab868e34f5
Your Name <you@example.com>
2021-08-28 10:19:13 +0200

    no

--- two/pagez/claimTwo.php
+++ two/pagez/claimTwo.php
@@ -5,7 +5,9 @@ session_start();
 include_once("settings.php");
 
 $sql = "insert into 42player (gameHash, color, first, sideKick) ";
-$sql .= " values ('".test_input($_SESSION['game']);."', '".test_input($_GET['color']);."', 'N', '".test_input($_GET['sideKick']);."');";
+$sql .= " values ('".test_input($_SESSION['game'])."', '".test_input($_GET['color'])."', 'N', '".test_input($_GET['sideKick'])."');";
+
+echo $sql;
 
 $conn->query($sql) or die($conn->error);
 
