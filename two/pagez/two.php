b2652fea635e4256b76ed1c5a8738368f33b7bc3
gonebergen <sander.gone.bergen@gmail.com>
2022-02-02 20:42:27 +0100

    radio

--- two/pagez/two.php
+++ two/pagez/two.php
@@ -226,7 +226,12 @@ $( document ).ready(function() {
 					</button>
 				</div>
 				<div class="modal-body">
-					<p>Modal body text goes here.</p>
+					<input type="radio" id="html" name="fav_language" value="HTML">
+					<label for="html">HTML</label><br>
+					<input type="radio" id="css" name="fav_language" value="CSS">
+					<label for="css">CSS</label><br>
+					<input type="radio" id="javascript" name="fav_language" value="JavaScript">
+					<label for="javascript">JavaScript</label>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-primary">Save changes</button>
