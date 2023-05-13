9b9fde7859bf8294a73d9e9909f5ea35695b02a2
gonebergen <sander.gone.bergen@gmail.com>
2023-05-13 22:13:53 +0200

    going to begin

--- two/pagez/twoWaiting.php
+++ two/pagez/twoWaiting.php
@@ -24,9 +24,10 @@ if(!isset($_SESSION['generated'])){
 		echo $_SESSION['json'] ."<br/>";
 		echo test_input($_SESSION['json']) ."<br/>";
 		echo addslashes(test_input($_SESSION['json'])) ."<br/>";
+		echo str_replace("\"", "\\\"", test_input($_SESSION['json'])) ."<br/>";
 
 		$sql = "insert into game42 (hash, json) ";
-		$sql .= " values ('".$gameHash."', '".addslashes(test_input($_SESSION['json']))."');";
+		$sql .= " values ('".$gameHash."', '".str_replace("\"", "\\\"", test_input($_SESSION['json']))."');";
 		
 	} else {
 
