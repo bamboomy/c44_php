85330a922233c19974026d7abb7dae3b3b3eb9ae
Sanpofio <sander.portfolio.1234@outlook.be>
2024-10-01 15:24:58 +0200

    bots: hositle

--- bots/pagez/claim.php
+++ bots/pagez/claim.php
@@ -8,12 +8,24 @@ $_SESSION['java_hash'] = md5($_SERVER['REMOTE_ADDR'] . microtime() . $_SESSION['
 
 $name = $_SESSION['name'];
 
+unset($colors[$_GET['color']]);
+
 if(!empty($_SESSION['color'])){
 
 	$name = "Friendly bot";
 	
 	$_SESSION['botColor'] = test_input($_GET['color']);
 	
+	$sql = "insert into colors_taken (game, color, name, java_hash) ";
+	$sql .= " values ('".$_SESSION['hash']."', '".$colors[0]."', 'Hostile Bot 1', '".$_SESSION['java_hash']."');";
+
+	$conn->query($sql) or die($conn->error);
+
+	$sql = "insert into colors_taken (game, color, name, java_hash) ";
+	$sql .= " values ('".$_SESSION['hash']."', '".$colors[1]."', 'Hostile Bot 2', '".$_SESSION['java_hash']."');";
+
+	$conn->query($sql) or die($conn->error);
+
 } else {
 	
 	$_SESSION['color'] = test_input($_GET['color']);
@@ -24,7 +36,5 @@ $sql = "insert into colors_taken (game, color, name, java_hash) ";
 $sql .= " values ('".$_SESSION['hash']."', '".test_input($_GET['color'])."', '".$name."', '".$_SESSION['java_hash']."');";
 
 $conn->query($sql) or die($conn->error);
-
-
 
 ?>
--- bots/pagez/reset.php
+++ bots/pagez/reset.php
@@ -10,6 +10,8 @@ unset($_SESSION['bot']);
 
 unset($_SESSION['botColor']);
 
+$colors = array("Green" => "Green", "Blue" => "Blue", "Red" => "Red", "Yellow" => "Yellow");
+
 header("Location: castles_one.php");
 
 ?>
